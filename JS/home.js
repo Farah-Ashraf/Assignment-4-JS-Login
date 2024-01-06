f4868b652b2123b3ec5fc902c19496bac4bde58a
farah <farahashraf268f@gmail.com>
2024-01-06 23:40:06 +0200

    fix redirection in home.js

--- JS/home.js
+++ JS/home.js
@@ -1,7 +1,7 @@
 const loggedUser = sessionStorage.getItem("loggedUser");
 
 if (!loggedUser) {
-  window.location.href = "../index.html";
+  window.location.replace("index.html");
 } else {
   let homeDiv = document.getElementById("homeContentDiv");
   homeDiv.innerHTML = `<h1 class="text-info my-4">Welcome ${loggedUser}</h1>`;
@@ -9,5 +9,5 @@ if (!loggedUser) {
 
 function logout() {
   sessionStorage.removeItem("loggedUser");
-  window.location.href = "../index.html";
+  window.location.replace("index.html");
 }
